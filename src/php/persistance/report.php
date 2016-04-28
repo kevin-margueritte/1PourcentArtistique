929121fafcdd599afd9778709a952ca97d762b4d
Pierrick Giuliani <giulianipierrick@gmail.com>
2016-04-28 10:27:31 +0200

    model report

--- src/php/persistance/report.php
+++ src/php/persistance/report.php
@@ -1,5 +1,9 @@
 <?php
 	class Report {
+
+		require_once 'connectionDB.php';
+
+
 		/**
 		Name of the video file
 		@var titleFileVideo
@@ -12,10 +16,31 @@
 		*/
 		private $nameArt;
 
+		private $db;
+
 		public function __construct ($nameArt, $titleFileVideo)
 		{
+			$this->db = connection();
 			$this->nameArt = $nameArt;
 			$this->titleFileVideo = $titleFileVideo;
+		}
+
+		/**
+		* Insert into report with title for an art
+		*/
+		public function save () {
+			$insert = $this->db->prepare("INSERT INTO REPORT(titleFileVideo, nameArt) 
+				VALUES (?, ?)");
+			return $insert->execute(array($this->titleFileVideo, $this->nameArt));
+		}
+
+		/**
+		* Test if the report exist in the database with title video for an art
+		*/
+		function exist() {
+			$exist = $this->db->prepare("SELECT 1 FROM REPORT WHERE titleFileVideo = ? AND nameArt = ?");
+			$exist->execute(array($this->titleFileVideo, $this->nameArt));
+			return count($exist->fetchAll()) >= 1;
 		}
 
 	    /**
