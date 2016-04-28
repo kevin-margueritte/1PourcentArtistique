4143aabee757d078947a8f7de6e242626b5c807b
Pierrick Giuliani <giulianipierrick@gmail.com>
2016-04-28 10:02:24 +0200

    model compose

--- src/php/persistance/compose.php
+++ src/php/persistance/compose.php
@@ -24,10 +24,22 @@
 			$this->nameArt = $nameArt;
 		}
 
+		/**
+		* Save in the database 
+		*/
 		public function save () {
 			$insert = $this->db->prepare("INSERT INTO COMPOSE(nameMaterial, nameArt) 
 				VALUES (?, ?)");
 			return $insert->execute(array($this->nameMaterial, $this->nameArt));
+		}
+
+		/**
+		* Test if exist in the database 
+		*/
+		function exist() {
+			$exist = $this->db->prepare("SELECT 1 FROM COMPOSE WHERE nameMaterial = ? AND nameArt = ?");
+			$exist->execute(array($this->nameMaterial, $this->nameArt));
+			return count($exist->fetchAll()) >= 1;
 		}
 	
 	    /**
